5ea0466135595c52ce52acb9a324641211886519
Hamza Benzaoui <hamzapdd@gmail.com>
2018-11-06 20:25:20 +0100

    connet to databae using knex

--- server.js
+++ server.js
@@ -4,6 +4,17 @@ const moment = require('moment');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 
+
+const knex = require('knex')({
+    client: 'pg',
+    connection: {
+        host:'127.0.0.1',
+        user:'postgres',
+        password:'',
+        database:'facrec',
+    }
+});
+
 const app = express();
 app.use(bodyparser.json());
 app.use(cors());
