ed0193b94bbb7669e00d38186f4f5083eaab0885
Hamza Benzaoui <hamzapdd@gmail.com>
2018-11-10 20:06:29 +0100

    connecting to databse on heroku server

--- server.js
+++ server.js
@@ -11,10 +11,8 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-        host:'127.0.0.1',
-        user:'postgres',
-        password:'',
-        database:'facerec',
+        connectionString: process.env.DATABASE_URL,
+        ssl: true,
     }
 });
 
